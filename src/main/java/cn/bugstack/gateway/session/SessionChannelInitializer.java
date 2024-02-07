ada65b095963e623cae7a953c32034286b2a487c
linqi <wkw4399xs@gmail.com>
2024-02-07 23:29:53 +0800

    feature: 会话服务调用处理

--- src/main/java/cn/bugstack/gateway/session/SessionChannelInitializer.java
+++ src/main/java/cn/bugstack/gateway/session/SessionChannelInitializer.java
@@ -1,6 +1,6 @@
-package cn.linqi.gateway.session;
+package cn.bugstack.gateway.session;
 
-import cn.linqi.gateway.session.handlers.SessionServerHandler;
+import cn.bugstack.gateway.session.handlers.SessionServerHandler;
 import io.netty.channel.ChannelInitializer;
 import io.netty.channel.ChannelPipeline;
 import io.netty.channel.socket.SocketChannel;
@@ -16,13 +16,19 @@ import io.netty.handler.codec.http.HttpResponseEncoder;
 
 public class SessionChannelInitializer extends ChannelInitializer<SocketChannel> {
 
+    private final Configuration configuration;
+
+    public SessionChannelInitializer(Configuration configuration) {
+        this.configuration = configuration;
+    }
+
     @Override
     protected void initChannel(SocketChannel channel) throws Exception {
         ChannelPipeline line = channel.pipeline();
         line.addLast(new HttpRequestDecoder());
         line.addLast(new HttpResponseEncoder());
         line.addLast(new HttpObjectAggregator(1024 * 1024));
-        line.addLast(new SessionServerHandler());
+        line.addLast(new SessionServerHandler(configuration));
     }
 
 }
