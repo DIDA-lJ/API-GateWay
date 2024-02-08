e368425c5d02c8c52174fd77f4df323ae576ef98
linqi <wkw4399xs@gmail.com>
2024-02-08 23:48:02 +0800

    feature: HTTP 调用指令枚举

---
+++ src/main/java/cn/bugstack/gateway/mapping/HttpCommandType.java
@@ -0,0 +1,14 @@
+package cn.bugstack.gateway.mapping;
+/**
+ * @author linqi
+ * @description HTTP 调用指令
+ */
+public enum HttpCommandType {
+
+    UNKNOWN,
+    GET,
+    POST,
+    PUT,
+    DELETE
+
+}
