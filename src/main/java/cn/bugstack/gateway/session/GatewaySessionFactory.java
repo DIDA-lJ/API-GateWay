04a51fe0de7d1be52b760510248f84e235581d9f
linqi <wkw4399xs@gmail.com>
2024-02-10 23:28:35 +0800

    feature: 网关会话工厂接口

---
+++ src/main/java/cn/bugstack/gateway/session/GatewaySessionFactory.java
@@ -0,0 +1,12 @@
+package cn.bugstack.gateway.session;
+
+/**
+ * @author linqi
+ * @version 1.0.0
+ * @description 网关会话工厂接口
+ */
+
+public interface GatewaySessionFactory {
+
+    GatewaySession openSession();
+}
