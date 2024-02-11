0c400ec81b47000f4dfd81cd332c298d8f798e50
linqi <wkw4399xs@gmail.com>
2024-02-11 22:44:21 +0800

    feature: 将连接(RPC\HTTP\其他)抽象为数据源 绑定调用方法

--- src/main/java/cn/bugstack/gateway/bind/MapperMethod.java
+++ src/main/java/cn/bugstack/gateway/bind/MapperMethod.java
@@ -14,11 +14,11 @@ import java.lang.reflect.Method;
 
 public class MapperMethod {
 
-    private String uri;
+    private String methodName;
     private final HttpCommandType command;
 
     public MapperMethod(String uri, Method method, Configuration configuration) {
-        this.uri = uri;
+        this.methodName = configuration.getHttpStatement(uri).getMethodName();
         this.command = configuration.getHttpStatement(uri).getHttpCommandType();
     }
 
@@ -26,7 +26,7 @@ public class MapperMethod {
         Object result = null;
         switch (command) {
             case GET:
-                result = session.get(uri, args);
+                result = session.get(methodName, args);
                 break;
             case POST:
                 break;
