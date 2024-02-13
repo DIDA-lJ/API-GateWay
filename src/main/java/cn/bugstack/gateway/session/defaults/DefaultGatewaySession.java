5f39021707c9fe4ac0cd70e97a835f93b0350e61
linqi <wkw4399xs@gmail.com>
2024-02-13 22:44:34 +0800

    feature:默认网关会话实现类

--- src/main/java/cn/bugstack/gateway/session/defaults/DefaultGatewaySession.java
+++ src/main/java/cn/bugstack/gateway/session/defaults/DefaultGatewaySession.java
@@ -1,16 +1,11 @@
 package cn.bugstack.gateway.session.defaults;
 
 import cn.bugstack.gateway.bind.IGenericReference;
+import cn.bugstack.gateway.datasource.Connection;
+import cn.bugstack.gateway.datasource.DataSource;
 import cn.bugstack.gateway.mapping.HttpStatement;
 import cn.bugstack.gateway.session.Configuration;
 import cn.bugstack.gateway.session.GatewaySession;
-import org.apache.dubbo.config.ApplicationConfig;
-import org.apache.dubbo.config.ReferenceConfig;
-import org.apache.dubbo.config.RegistryConfig;
-import org.apache.dubbo.config.bootstrap.DubboBootstrap;
-import org.apache.dubbo.config.utils.ReferenceConfigCache;
-import org.apache.dubbo.rpc.service.GenericService;
-
 /**
  * @author linqi
  * @version 1.0.0
@@ -20,37 +15,23 @@ import org.apache.dubbo.rpc.service.GenericService;
 public class DefaultGatewaySession implements GatewaySession {
 
     private Configuration configuration;
+    private String uri;
+    private DataSource dataSource;
 
-    public DefaultGatewaySession(Configuration configuration) {
+    public DefaultGatewaySession(Configuration configuration, String uri, DataSource dataSource) {
         this.configuration = configuration;
+        this.uri = uri;
+        this.dataSource = dataSource;
     }
 
     @Override
-    public Object get(String uri, Object parameter) {
-
-        /* 以下这部分内容，后续拆到执行器中处理 */
-
-        // 配置信息
-        HttpStatement httpStatement = configuration.getHttpStatement(uri);
-        String application = httpStatement.getApplication();
-        String interfaceName = httpStatement.getInterfaceName();
-
-        // 获取基础服务（创建成本较高，内存存放获取）
-        ApplicationConfig applicationConfig = configuration.getApplicationConfig(application);
-        RegistryConfig registryConfig = configuration.getRegistryConfig(application);
-        ReferenceConfig<GenericService> reference = configuration.getReferenceConfig(interfaceName);
-        // 构建Dubbo服务
-        DubboBootstrap bootstrap = DubboBootstrap.getInstance();
-        bootstrap.application(applicationConfig).registry(registryConfig).reference(reference).start();
-        // 获取泛化调用服务
-        ReferenceConfigCache cache = ReferenceConfigCache.getCache();
-        GenericService genericService = cache.get(reference);
-
-        return genericService.$invoke(httpStatement.getMethodName(), new String[]{"java.lang.String"}, new Object[]{"答案说明所有"});
+    public Object get(String methodName, Object parameter) {
+        Connection connection = dataSource.getConnection();
+        return connection.execute(methodName, new String[]{"java.lang.String"}, new String[]{"name"}, new Object[]{parameter});
     }
 
     @Override
-    public IGenericReference getMapper(String uri) {
+    public IGenericReference getMapper() {
         return configuration.getMapper(uri, this);
     }
 
