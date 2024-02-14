9de33d947fd90250fff3ecd0ef34c9a493c06a7b
linqi <wkw4399xs@gmail.com>
2024-02-14 21:50:44 +0800

    feature: 映射代理调用

--- src/main/java/cn/bugstack/gateway/bind/MapperProxy.java
+++ src/main/java/cn/bugstack/gateway/bind/MapperProxy.java
@@ -6,6 +6,7 @@ import net.sf.cglib.proxy.MethodProxy;
 import org.apache.dubbo.rpc.service.GenericService;
 
 import java.lang.reflect.Method;
+import java.util.Map;
 
 /**
  * @author linqi
@@ -27,7 +28,7 @@ public class MapperProxy implements MethodInterceptor {
     public Object intercept(Object obj, Method method, Object[] args, MethodProxy proxy) throws Throwable {
         MapperMethod linkMethod = new MapperMethod(uri, method, gatewaySession.getConfiguration());
         // 暂时只获取第0个参数
-        return linkMethod.execute(gatewaySession, args[0]);
+        return linkMethod.execute(gatewaySession, (Map<String, Object>) args[0]);
     }
 
 }
