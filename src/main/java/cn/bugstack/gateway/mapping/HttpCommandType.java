ede9a39251df5d75764efceb78ab4ec927f0922d
linqi <wkw4399xs@gmail.com>
2024-02-14 22:03:42 +0800

    feature: HTTP请求参数解析:HTTP 调用方式指令注释补充

--- src/main/java/cn/bugstack/gateway/mapping/HttpCommandType.java
+++ src/main/java/cn/bugstack/gateway/mapping/HttpCommandType.java
@@ -1,14 +1,31 @@
 package cn.bugstack.gateway.mapping;
+
+import org.omg.CORBA.UNKNOWN;
+
 /**
  * @author linqi
- * @description HTTP 调用指令
+ * @description HTTP 调用方式指令
  */
 public enum HttpCommandType {
-
+    /**
+     * 除了 Get、Post、Put、Delete 以外的 HTTP 请求方式，如 Head,Patch 等
+     */
     UNKNOWN,
+    /**
+     * Get 请求
+     */
     GET,
+    /**
+     * Post 请求
+     */
     POST,
+    /**
+     * Put 请求
+     */
     PUT,
+    /**
+     * Delete 请求
+     */
     DELETE
 
 }
