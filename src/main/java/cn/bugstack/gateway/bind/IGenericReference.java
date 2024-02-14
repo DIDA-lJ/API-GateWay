7c93529c457d2eb54a911fec3c4ad9eab1d39ade
linqi <wkw4399xs@gmail.com>
2024-02-14 22:31:39 +0800

    feature: HTTP请求参数解析:网关调用方传入参数的方式改成 map 传入

--- src/main/java/cn/bugstack/gateway/bind/IGenericReference.java
+++ src/main/java/cn/bugstack/gateway/bind/IGenericReference.java
@@ -1,5 +1,7 @@
 package cn.bugstack.gateway.bind;
 
+import java.util.Map;
+
 /**
  * @InterfaceName IGenericReference
  * @Description 统一泛化调用接口 (继承 core-03)
@@ -10,8 +12,8 @@ package cn.bugstack.gateway.bind;
 public interface IGenericReference {
     /**
      * 提供一个标准的 String 的入参和出参类型的泛化调用接口
-     * @param args 标准的 String 的入参
-     * @return String 出参
+     * @param params 传入参数，以 map 的方式传入执行调用
+     * @return 执行结果的串
      */
-    String $invoke(String args);
+    String $invoke(Map<String, Object> params);
 }
