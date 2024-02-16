1d5dd8d99a476d670af9d1608fc7922b2645b9e7
linqi <wkw4399xs@gmail.com>
2024-02-16 19:16:50 +0800

    feature: 权限认证组件(Shiro+Jwt)引入: 权限验证添加解析

--- src/main/java/cn/bugstack/gateway/socket/agreement/RequestParser.java
+++ src/main/java/cn/bugstack/gateway/socket/agreement/RequestParser.java
@@ -35,9 +35,7 @@ public class RequestParser {
         String uri = request.uri();
         int idx = uri.indexOf("?");
         uri = idx > 0 ? uri.substring(0, idx) : uri;
-        if (uri.equals("/favicon.ico")) {
-            return null;
-        }
+        if (uri.equals("/favicon.ico")) return null;
         return uri;
     }
 
@@ -84,14 +82,23 @@ public class RequestParser {
         throw new RuntimeException("未实现的请求类型 HttpMethod：" + method);
     }
 
+    /*
+     * POST /wg/activity/insert HTTP/1.1
+     * Content-Type: application/json
+     * User-Agent: PostmanRuntime/7.29.2
+     * Postman-Token: f535ef0b-a257-4bf8-9542-4291d925de32
+     * Host: localhost:7397
+     * Accept-Encoding: gzip, deflate, br
+     * Connection: keep-alive
+     * Content-Length: 45
+     */
     private String getContentType() {
+        System.out.println(request.headers());
         Optional<Map.Entry<String, String>> header = request.headers().entries().stream().filter(
                 val -> val.getKey().equals("Content-Type")
         ).findAny();
         Map.Entry<String, String> entry = header.orElse(null);
-        if (null == entry) {
-            return "none";
-        }
+        if (null == entry) return "none";
         // application/json、multipart/form-data;
         String contentType = entry.getValue();
         int idx = contentType.indexOf(";");
@@ -103,3 +110,4 @@ public class RequestParser {
     }
 
 }
+
