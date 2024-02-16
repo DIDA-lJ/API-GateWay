52f9d2431d446bdca025555c87aad484fc117921
linqi <wkw4399xs@gmail.com>
2024-02-16 20:02:47 +0800

    feature: 网关会话鉴权处理: 执行器抽象基类

--- src/main/java/cn/bugstack/gateway/executor/BaseExecutor.java
+++ src/main/java/cn/bugstack/gateway/executor/BaseExecutor.java
@@ -1,10 +1,10 @@
 package cn.bugstack.gateway.executor;
 
 import cn.bugstack.gateway.datasource.Connection;
-import cn.bugstack.gateway.executor.result.GatewayResult;
+import cn.bugstack.gateway.executor.result.SessionResult;
+
 import cn.bugstack.gateway.mapping.HttpStatement;
 import cn.bugstack.gateway.session.Configuration;
-import cn.bugstack.gateway.session.GatewaySession;
 import cn.bugstack.gateway.type.SimpleTypeRegistry;
 import com.alibaba.fastjson.JSON;
 import org.slf4j.Logger;
@@ -18,7 +18,6 @@ import java.util.Map;
  * @version 1.0.0
  * @description 执行器抽象基类
  */
-
 public abstract class BaseExecutor implements Executor {
 
     private Logger logger = LoggerFactory.getLogger(BaseExecutor.class);
@@ -32,7 +31,7 @@ public abstract class BaseExecutor implements Executor {
     }
 
     @Override
-    public GatewayResult exec(HttpStatement httpStatement, Map<String, Object> params) throws Exception {
+    public SessionResult exec(HttpStatement httpStatement, Map<String, Object> params) throws Exception {
         // 参数处理；后续的一些参数校验也可以在这里封装。
         String methodName = httpStatement.getMethodName();
         String parameterType = httpStatement.getParameterType();
@@ -42,13 +41,14 @@ public abstract class BaseExecutor implements Executor {
         // 抽象方法
         try {
             Object data = doExec(methodName, parameterTypes, args);
-            return GatewayResult.buildSuccess(data);
+            return SessionResult.buildSuccess(data);
         } catch (Exception e) {
-            return GatewayResult.buildError(e.getMessage());
+            return SessionResult.buildError(e.getMessage());
         }
     }
 
     protected abstract Object doExec(String methodName, String[] parameterTypes, Object[] args);
 
 }
+
 
