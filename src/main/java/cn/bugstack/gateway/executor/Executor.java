543b6ea3f919d36e0a47736e46aa72e0f6e688a3
linqi <wkw4399xs@gmail.com>
2024-02-16 20:03:21 +0800

    feature: 网关会话鉴权处理: 执行器

--- src/main/java/cn/bugstack/gateway/executor/Executor.java
+++ src/main/java/cn/bugstack/gateway/executor/Executor.java
@@ -1,6 +1,6 @@
 package cn.bugstack.gateway.executor;
 
-import cn.bugstack.gateway.executor.result.GatewayResult;
+import cn.bugstack.gateway.executor.result.SessionResult;
 import cn.bugstack.gateway.mapping.HttpStatement;
 
 import java.util.Map;
@@ -14,6 +14,6 @@ import java.util.Map;
  */
 public interface Executor {
 
-    GatewayResult exec(HttpStatement httpStatement, Map<String, Object> params) throws Exception;
+    SessionResult exec(HttpStatement httpStatement, Map<String, Object> params) throws Exception;
 
 }
