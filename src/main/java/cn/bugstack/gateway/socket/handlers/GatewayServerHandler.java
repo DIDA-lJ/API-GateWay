863fa6ca61bfc391accd9a86c31444f609cff483
linqi <wkw4399xs@gmail.com>
2024-02-16 20:24:47 +0800

    feature: 网关会话鉴权处理: 网络协议处理器

--- src/main/java/cn/bugstack/gateway/socket/handlers/GatewayServerHandler.java
+++ src/main/java/cn/bugstack/gateway/socket/handlers/GatewayServerHandler.java
@@ -1,21 +1,20 @@
 package cn.bugstack.gateway.socket.handlers;
 
-import cn.bugstack.gateway.bind.IGenericReference;
-import cn.bugstack.gateway.session.GatewaySession;
-import cn.bugstack.gateway.session.defaults.DefaultGatewaySessionFactory;
+
+import cn.bugstack.gateway.mapping.HttpStatement;
+import cn.bugstack.gateway.session.Configuration;
 import cn.bugstack.gateway.socket.BaseHandler;
+import cn.bugstack.gateway.socket.agreement.AgreementConstants;
+import cn.bugstack.gateway.socket.agreement.GatewayResultMessage;
 import cn.bugstack.gateway.socket.agreement.RequestParser;
 import cn.bugstack.gateway.socket.agreement.ResponseParser;
-import com.alibaba.fastjson.JSON;
-import com.alibaba.fastjson.serializer.SerializerFeature;
 import io.netty.channel.Channel;
 import io.netty.channel.ChannelHandlerContext;
-import io.netty.handler.codec.http.*;
+import io.netty.handler.codec.http.DefaultFullHttpResponse;
+import io.netty.handler.codec.http.FullHttpRequest;
+import io.netty.util.AttributeKey;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-
-import java.util.Map;
-
 import io.netty.handler.codec.http.FullHttpRequest;
 
 /**
@@ -27,30 +26,35 @@ public class GatewayServerHandler extends BaseHandler<FullHttpRequest> {
 
     private final Logger logger = LoggerFactory.getLogger(GatewayServerHandler.class);
 
-    private final DefaultGatewaySessionFactory gatewaySessionFactory;
+    private final Configuration configuration;
 
-    public GatewayServerHandler(DefaultGatewaySessionFactory gatewaySessionFactory) {
-        this.gatewaySessionFactory = gatewaySessionFactory;
+    public GatewayServerHandler(Configuration configuration) {
+        this.configuration = configuration;
     }
 
     @Override
     protected void session(ChannelHandlerContext ctx, final Channel channel, FullHttpRequest request) {
-        logger.info("网关接收请求 uri：{} method：{}", request.uri(), request.method());
-        // 1. 解析请求参数
-        RequestParser requestParser = new RequestParser(request);
-        String uri = requestParser.getUri();
-        if (null == uri) return;
-        Map<String, Object> args = requestParser.parse();
+        logger.info("网关接收请求【全局】 uri：{} method：{}", request.uri(), request.method());
+        try {
+            // 1. 解析参数
+            RequestParser requestParser = new RequestParser(request);
+            String uri = requestParser.getUri();
 
-        // 2. 调用会话服务
-        GatewaySession gatewaySession = gatewaySessionFactory.openSession(uri);
-        IGenericReference reference = gatewaySession.getMapper();
-        Object result = reference.$invoke(args);
+            // 2. 保存信息；HttpStatement、Header=token
+            HttpStatement httpStatement = configuration.getHttpStatement(uri);
+            channel.attr(AgreementConstants.HTTP_STATEMENT).set(httpStatement);
 
-        // 3. 封装返回结果
-        DefaultFullHttpResponse response = new ResponseParser().parse(result);
-        channel.writeAndFlush(response);
+            // 3. 放行服务
+            request.retain();
+            ctx.fireChannelRead(request);
+        } catch (Exception e) {
+            // 4. 封装返回结果
+            DefaultFullHttpResponse response = new ResponseParser().parse(GatewayResultMessage.buildError(AgreementConstants.ResponseCode._500.getCode(), "网关协议调用失败！" + e.getMessage()));
+            channel.writeAndFlush(response);
+        }
+
     }
 
 }
+
 
