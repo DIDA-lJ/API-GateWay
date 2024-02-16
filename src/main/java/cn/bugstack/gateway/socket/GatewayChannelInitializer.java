8699683453a8e9983f92a93444e31e89d91170a8
linqi <wkw4399xs@gmail.com>
2024-02-16 20:28:53 +0800

    feature: 网关会话鉴权处理: 会话管道初始化类

--- src/main/java/cn/bugstack/gateway/socket/GatewayChannelInitializer.java
+++ src/main/java/cn/bugstack/gateway/socket/GatewayChannelInitializer.java
@@ -1,13 +1,17 @@
 package cn.bugstack.gateway.socket;
 
+import cn.bugstack.gateway.session.Configuration;
 import cn.bugstack.gateway.session.defaults.DefaultGatewaySessionFactory;
+import cn.bugstack.gateway.socket.handlers.AuthorizationHandler;
 import cn.bugstack.gateway.socket.handlers.GatewayServerHandler;
+import cn.bugstack.gateway.socket.handlers.ProtocolDataHandler;
 import io.netty.channel.ChannelInitializer;
 import io.netty.channel.ChannelPipeline;
 import io.netty.channel.socket.SocketChannel;
 import io.netty.handler.codec.http.HttpObjectAggregator;
 import io.netty.handler.codec.http.HttpRequestDecoder;
 import io.netty.handler.codec.http.HttpResponseEncoder;
+
 
 
 /**
@@ -17,9 +21,11 @@ import io.netty.handler.codec.http.HttpResponseEncoder;
  */
 public class GatewayChannelInitializer extends ChannelInitializer<SocketChannel> {
 
+    private final Configuration configuration;
     private final DefaultGatewaySessionFactory gatewaySessionFactory;
 
-    public GatewayChannelInitializer(DefaultGatewaySessionFactory gatewaySessionFactory) {
+    public GatewayChannelInitializer(Configuration configuration, DefaultGatewaySessionFactory gatewaySessionFactory) {
+        this.configuration = configuration;
         this.gatewaySessionFactory = gatewaySessionFactory;
     }
 
@@ -29,7 +35,9 @@ public class GatewayChannelInitializer extends ChannelInitializer<SocketChannel>
         line.addLast(new HttpRequestDecoder());
         line.addLast(new HttpResponseEncoder());
         line.addLast(new HttpObjectAggregator(1024 * 1024));
-        line.addLast(new GatewayServerHandler(gatewaySessionFactory));
+        line.addLast(new GatewayServerHandler(configuration));
+        line.addLast(new AuthorizationHandler(configuration));
+        line.addLast(new ProtocolDataHandler(gatewaySessionFactory));
     }
 
 }
